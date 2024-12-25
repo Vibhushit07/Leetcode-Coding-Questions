f516ac3439ecbdfa64178ada2ee603f97d4c7cf2
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2024-12-26 01:20:52 +0530

    Time: 16 ms (6.53%), Space: 43.9 MB (76.79%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -19,13 +19,10 @@ class Solution {
     }
     
     int updateChar(char[] chars, int len, int count, int i) {
-        System.out.println("count " + count);
+        
         chars[len] = chars[i];
         
-        if(count > 1 && count <= 9) {
-            chars[len + 1] = String.valueOf(count).charAt(0);
-            len++;
-        } else if(count > 9) {
+        if(count > 1 ) {
             char co[] = String.valueOf(count).toCharArray();
             for(int j = 0; j < co.length; j++) {
                 chars[len + j + 1] = co[j];
