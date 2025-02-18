28dedbea116faad4a3fc85d90636997ba6291223
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2025-02-18 23:04:57 +0530

    Time: 2 ms (29.67%), Space: 41.6 MB (62.62%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -1,51 +1,18 @@
 class Solution {
     public String smallestNumber(String pattern) {
         String res = "";
-        int i = 0;
-        int countD = 0;
-        String d = "";
+        Stack<Integer> st = new Stack<>();
 
-        while (i < pattern.length()) {
+        for(int i = 1; i <= pattern.length() + 1; i++) {
+            st.push(i);
 
-            if (pattern.charAt(i) == 'I') {
-                if (countD != 0) {
-                    d += String.valueOf(i + 1);
-                    res += reverse(d);
-                    d = "";
-                    countD = 0;
-                } else {
-                    res += String.valueOf(i + 1);
+            if(i == pattern.length() + 1 || pattern.charAt(i - 1) == 'I') {
+                while(!st.isEmpty()) {
+                    res += String.valueOf(st.pop());
                 }
-            } else {
-                d += String.valueOf(i + 1);
-                countD++;
-            }
-            i++;
-        }
-
-        if(!d.equals("")) {
-            d += String.valueOf(i + 1);
-            res += reverse(d);
-        } else {
-            res += String.valueOf(i + 1);
+            } 
         }
 
         return res;
-    }
-
-    private String reverse(String str) {
-        char s[] = str.toCharArray();
-
-        int i = 0, j = s.length - 1;
-
-        while(i < j) {
-            char c = s[i];
-            s[i] = s[j];
-            s[j] = c;
-            j--;
-            i++;
-        }
-
-        return new String(s);
     }
 }
