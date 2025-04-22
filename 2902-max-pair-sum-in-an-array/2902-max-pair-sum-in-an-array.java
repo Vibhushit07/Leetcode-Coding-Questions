b53e0091685f5ef0a7dfaf7b26d3ed069b2a11f3
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2025-04-22 15:44:55 +0530

    Time: 6 ms (54.5%), Space: 45.1 MB (38.25%) - LeetHub

---
+++ 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.java
@@ -0,0 +1,92 @@
+class Solution {
+
+    class Pair {
+        int first, second;
+
+        Pair(int f, int s) {
+            first = f;
+            second = s;
+        }
+
+        int getFirst() {
+            return first;
+        }
+
+        int getSecond() {
+            return second;
+        }
+    }
+
+    public int maxSum(int[] nums) {
+        // Arrays.sort(nums);
+        int n = nums.length, maxSum = -1;
+        HashMap<Integer, Pair> digits = getLargestDigitsPair(nums);
+
+        for(Map.Entry<Integer, Pair> map : digits.entrySet()) {
+             Pair pair = map.getValue();
+            // int size = list.size();
+            if(pair.first != -1) {
+                maxSum = Math.max(maxSum, pair.getFirst() + pair.getSecond());
+            }
+        }
+
+        return maxSum;
+
+    }
+    
+    private HashMap<Integer, Pair> getLargestDigitsPair(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Pair> digits = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+
+            int lD = -1, num = nums[i];
+
+            while(num > 0) {
+                int d = num % 10;
+                num /= 10;
+
+                lD = Math.max(d, lD);
+            }
+
+            Pair pair = digits.getOrDefault(lD, new Pair(-1, -1));
+
+            if(pair.getSecond() < nums[i]) {
+                pair.first = pair.second;
+                pair.second = nums[i];
+
+            } else if (pair.getFirst() < nums[i]) {
+                pair.first = nums[i];
+            }
+
+            digits.put(lD, pair);
+        }
+
+        return digits;
+    }
+
+    private HashMap<Integer, List<Integer>> getLargestDigits(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, List<Integer>> digits = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+
+            int lD = -1, num = nums[i];
+
+            while(num > 0) {
+                int d = num % 10;
+                num /= 10;
+
+                lD = Math.max(d, lD);
+            }
+
+            List<Integer> list = digits.getOrDefault(lD, new ArrayList<>());
+            list.add(nums[i]);
+            digits.put(lD, list);
+        }
+
+        return digits;
+    }
+}
+
+// 162, 1614, 2536, 3344
