10ec6867b3ff135f6e1fcef8e6c6d7f38c8b61db
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2025-05-18 22:52:57 +0530

    Time: 203 ms (22.22%), Space: 43.5 MB (71.6%) - LeetHub

---
+++ 2061-painting-a-grid-with-three-different-colors/2061-painting-a-grid-with-three-different-colors.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    private final int MOD = 1_000_000_007;
+    public int colorTheGrid(int m, int n) {
+
+        char color[] = new char[]{'R', 'G', 'B'};
+        int result = 0;
+
+        List<String> colorComb = new ArrayList<>();
+        getComb(colorComb, color, m, "", '0');
+        int dp[][] = new int[n][colorComb.size()];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        for(int i = 0; i < colorComb.size(); i++) {
+            result = (result + solve(dp, colorComb, n - 1, m, i)) % MOD;
+        }
+
+        return result;
+    }
+
+    private void getComb(List<String> colorComb, char color[], int rows, String comb, char prevColor) {
+        if(rows == 0) {
+            colorComb.add(comb);
+        } else {
+            for(char c : color) {
+                if(c != prevColor) {
+                    getComb(colorComb, color, rows - 1, comb + c, c);
+                }
+            }
+        }
+    }
+
+    private int solve(int dp[][], List<String> colorComb, int cols, int m, int prevCol) {
+        if(cols == 0) {
+            return 1;
+        }
+
+        if(dp[cols][prevCol] != -1) return dp[cols][prevCol];
+        
+        String comb = colorComb.get(prevCol);
+        int ways = 0;
+
+        for(int i = 0; i < colorComb.size(); i++) {
+            boolean valid = true;
+            String curr = colorComb.get(i);
+            for(int j = 0; j < m; j++) {
+                if(comb.charAt(j) == curr.charAt(j)) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if(valid) {
+                ways = (ways + solve(dp, colorComb, cols - 1, m, i)) % MOD;
+            } 
+        }
+
+        dp[cols][prevCol] = ways;
+
+        return ways;
+    }
+}
