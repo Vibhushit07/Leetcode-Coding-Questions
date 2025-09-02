6277e7c88a60ebad18e464ab3ccf6f318a4cbca7
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2025-09-02 21:05:55 +0530

    Time: 5 ms (100%), Space: 44.7 MB (61.19%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
@@ -1,36 +1,25 @@
 class Solution {
-
-    boolean checkValid(int[][] points, int i, int j, int n) {
-        int x1 = points[i][0], x2 = points[j][0];
-        int y1 = points[i][1], y2 = points[j][1];
-
-        for(int k = 0; k < n; k++) {
-            if(k != i && k != j) {
-                int x = points[k][0], y = points[k][1];
-                if(x >= x1 && x <= x2 && y <= y1 && y >= y2) 
-                    return false;
-            }
-        }
-
-        return true;
-    }
- 
     public int numberOfPairs(int[][] points) {
         int n = points.length, count = 0;
 
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(b[1], a[1]);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
+
         for(int i = 0; i < n; i++) {
+            
+            int maxY = Integer.MIN_VALUE;
+
             for(int j = i + 1; j < n; j++) {
                 int x1 = points[i][0], x2 = points[j][0];
                 int y1 = points[i][1], y2 = points[j][1];
 
-                if(x1 <= x2 && y1 >= y2) {
-                    if(checkValid(points, i, j, n)) {
-                        count++;
-                    }
-                } else if (x1 >= x2 && y1 <= y2) {
-                    if(checkValid(points, j, i, n)) {
-                        count++;
-                    }
+                if(y2 <= y1 && y2 > maxY) {
+                    maxY = y2;
+                    count++;
                 }
             }
         }
