f8fd07b64b900a85477fa31e6d9503ab1a602d7d
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2025-10-08 11:24:47 +0530

    Time: 3 ms (95.83%), Space: 44.6 MB (38.11%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,39 +1,48 @@
 class Solution {
-    public int swimInWater(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        
-        int lo = grid[0][0], hi = 0;
-        for (int[] row : grid)
-            for (int val : row)
-                hi = Math.max(hi, val);
-        
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (possible(grid, mid, m, n, directions)) hi = mid;
-            else lo = mid + 1;
+
+    boolean isReachable(int i, int j, int n, int[][] grid, boolean isVisited[][], int x[], int y[], int mid) {
+        if(i < 0 || j < 0 || i >= n || j >= n || isVisited[i][j] || grid[i][j] > mid) {
+            return false;
         }
-        return lo;
-    }
-    
-    private boolean possible(int[][] grid, int mid, int m, int n, int[][] directions) {
-        if (grid[0][0] > mid) return false;
-        boolean[][] seen = new boolean[m][n];
-        return dfs(grid, 0, 0, mid, seen, m, n, directions);
-    }
-    
-    private boolean dfs(int[][] grid, int r, int c, int mid, boolean[][] seen, int m, int n, int[][] directions) {
-        if (r == m-1 && c == n-1) return true;
-        seen[r][c] = true;
-        
-        for (int[] dir : directions) {
-            int nr = r + dir[0], nc = c + dir[1];
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !seen[nr][nc]) {
-                if (grid[nr][nc] <= mid) {
-                    if (dfs(grid, nr, nc, mid, seen, m, n, directions)) return true;
-                }
+
+        isVisited[i][j] = true;
+
+        if(i == n - 1 && j == n - 1) {
+            return true;
+        }
+
+        for(int k = 0; k < 4; k++) {
+            int ix = i + x[k];
+            int jy = j + y[k];
+
+            if(isReachable(ix , jy, n, grid, isVisited, x, y, mid)) {
+                return true;
             }
         }
+
         return false;
+    }
+
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
+
+        int x[] = {0, 0, -1, 1};
+        int y[] = {-1, 1, 0, 0};
+
+        int minTime = 0, l = 0, r = n * n - 1;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            boolean isVisited[][] = new boolean[n][n];
+
+            if(isReachable(0 , 0, n, grid, isVisited, x, y, mid)) {
+                minTime = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return minTime;
     }
 }
