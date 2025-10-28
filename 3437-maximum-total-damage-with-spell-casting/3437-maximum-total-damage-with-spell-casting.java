5f4d97ae8d4fa91de248e93e04514c3c986d6530
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2025-10-28 22:48:48 +0530

    Time: 285 ms (42.09%), Space: 62.1 MB (57.7%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
@@ -1,6 +1,7 @@
 class Solution {
     public long maximumTotalDamage(int[] power) {
         Map<Long, Long>freq = new HashMap<>();
+        long result = 0;
         
         for(int num : power) {
             freq.put((long)num, freq.getOrDefault((long)num, 0l) + 1);
@@ -8,11 +9,22 @@ class Solution {
 
         List<Long> nums = new ArrayList<>(freq.keySet());
         Collections.sort(nums);
+        int n = nums.size();
 
         long[] dp = new long[nums.size()];
         Arrays.fill(dp, -1);
 
-        return solve(nums, dp, freq, 0);
+        for(int i = n - 1; i >= 0; i--) {
+            long skip = (i + 1 >= n) ? 0 : dp[i + 1];
+
+            int j = lowerBound(nums, i + 1, nums.get(i) + 3);
+            long take = nums.get(i) * freq.get(nums.get(i)) + ((j >= n) ? 0 : dp[j]);
+
+            dp[i] = Math.max(skip, take);
+            result = dp[i];
+        }
+
+        return result;
     }
 
     private int lowerBound(List<Long> nums, int start, long target) {
@@ -28,24 +40,5 @@ class Solution {
         }
 
         return l;
-    }
-
-    private long solve(List<Long> nums, long[] dp, Map<Long, Long>freq, int i) {
-        if(i >= nums.size()) {
-            return 0;
-        }
-
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-
-        long skip = solve(nums, dp, freq, i + 1);
-
-        int j = lowerBound(nums, i + 1, nums.get(i) + 3);
-        long take = nums.get(i) * freq.get(nums.get(i)) + solve(nums, dp, freq, j);
-
-        dp[i] = Math.max(skip, take);
-
-        return dp[i];
     }
 }
