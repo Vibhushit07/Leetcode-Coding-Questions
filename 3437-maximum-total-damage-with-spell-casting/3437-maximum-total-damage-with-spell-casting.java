66e073ba648fb831905a1438f331cc4853dd0107
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2025-10-28 22:37:43 +0530

    Time: 338 ms (23.91%), Space: 80 MB (7.68%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
@@ -1,35 +1,51 @@
 class Solution {
     public long maximumTotalDamage(int[] power) {
-        Map<Integer, Long> freq = new HashMap<>();
-        for (int p : power) freq.put(p, freq.getOrDefault(p, 0L) + 1);
-
-        List<Integer> keys = new ArrayList<>(freq.keySet());
-        Collections.sort(keys);
-
-        int n = keys.size();
-        long[] dp = new long[n];
-        dp[0] = freq.get(keys.get(0)) * keys.get(0);
-
-        for (int i = 1; i < n; i++) {
-            long take = freq.get(keys.get(i)) * keys.get(i);
-            int prev = binarySearch(keys, i - 1, keys.get(i) - 3);
-            if (prev >= 0) take += dp[prev];
-            dp[i] = Math.max(dp[i - 1], take);
+        Map<Long, Long>freq = new HashMap<>();
+        
+        for(int num : power) {
+            freq.put((long)num, freq.getOrDefault((long)num, 0l) + 1);
         }
 
-        return dp[n - 1];
+        List<Long> nums = new ArrayList<>(freq.keySet());
+        Collections.sort(nums);
+
+        long[] dp = new long[nums.size()];
+        Arrays.fill(dp, -1);
+
+        return solve(nums, dp, freq, 0);
     }
 
-    private int binarySearch(List<Integer> keys, int end, int value) {
-        int l = 0, r = end, ans = -1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (keys.get(mid) <= value) {
-                ans = mid;
+    private int lowerBound(List<Long> nums, int start, long target) {
+        int l = start, r = nums.size();
+
+        while(l < r) {
+            int mid = (r + l) / 2;
+            if(nums.get(mid) < target) {
                 l = mid + 1;
+            } else {
+                r = mid;
             }
-            else r = mid - 1;
         }
-        return ans;
+
+        return l;
+    }
+
+    private long solve(List<Long> nums, long[] dp, Map<Long, Long>freq, int i) {
+        if(i >= nums.size()) {
+            return 0;
+        }
+
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+
+        long skip = solve(nums, dp, freq, i + 1);
+
+        int j = lowerBound(nums, i + 1, nums.get(i) + 3);
+        long take = nums.get(i) * freq.get(nums.get(i)) + solve(nums, dp, freq, j);
+
+        dp[i] = Math.max(skip, take);
+
+        return dp[i];
     }
 }
