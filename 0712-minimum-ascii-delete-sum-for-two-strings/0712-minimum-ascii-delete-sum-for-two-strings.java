57f2f689825f5c4ba1d929b9cbef8b71fc085ac4
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2026-01-10 15:58:32 +0530

    Time: 22 ms (42.47%), Space: 46.6 MB (42.92%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,41 +1,32 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
 
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
-        int[][] memo = new int[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = 0;
 
-        for(int[] row : memo) {
-            Arrays.fill(row, -1);
+        for(int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
         }
 
-        return solve(s1, s2, 0, 0, memo);
-    }
-
-    private int solve(String s1, String s2, int i, int j, int[][] memo) {
-
-        if(i >= s1.length() && j >= s2.length()) {
-            return 0;
+        for(int j = 1; j <= m; j++) {
+            dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
         }
 
-        if(i == s1.length() && j < s2.length()) {
-            return memo[i][j] = s2.charAt(j) + solve(s1, s2, i, j + 1, memo);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int delete1 = dp[i - 1][j] + s1.charAt(i - 1);
+                    int delete2 = dp[i][j - 1] + s2.charAt(j - 1);
+
+                    dp[i][j] = Math.min(delete1, delete2);
+                }
+            }
         }
 
-        if(i < s1.length() && j == s2.length()) {
-            return memo[i][j] = s1.charAt(i) + solve(s1, s2, i + 1, j, memo);
-        }
-
-        if(memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        if(s1.charAt(i) == s2.charAt(j)) {
-            return memo[i][j] = solve(s1, s2, i + 1, j + 1, memo);
-        }
-
-        int skip1 = s1.charAt(i) + solve(s1, s2, i + 1, j, memo);
-        int skip2 = s2.charAt(j) + solve(s1, s2, i, j + 1, memo);
-
-        return memo[i][j] = Math.min(skip1, skip2);
+        return dp[n][m];
     }
 }
