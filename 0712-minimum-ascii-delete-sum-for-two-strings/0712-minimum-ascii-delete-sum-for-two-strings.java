770218758c3a80aa88975b8d00ed65dd73e8d787
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2026-01-10 15:50:10 +0530

    Time: 41 ms (5.22%), Space: 46.5 MB (58.72%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumDeleteSum(String s1, String s2) {
 
 
-        int[][] memo = new int[s1.length()][s2.length()];
+        int[][] memo = new int[s1.length() + 1][s2.length() + 1];
 
         for(int[] row : memo) {
             Arrays.fill(row, -1);
@@ -13,16 +13,16 @@ class Solution {
 
     private int solve(String s1, String s2, int i, int j, int[][] memo) {
 
-        if(i < s1.length() && j >= s2.length()) {
-            return deleteAll(s1, i);
-        }
-
-        if(i >= s1.length() && j < s2.length()) {
-            return deleteAll(s2, j);
-        }
-
-        if(i >= s1.length() || j >= s2.length()) {
+        if(i >= s1.length() && j >= s2.length()) {
             return 0;
+        }
+
+        if(i == s1.length() && j < s2.length()) {
+            return memo[i][j] = s2.charAt(j) + solve(s1, s2, i, j + 1, memo);
+        }
+
+        if(i < s1.length() && j == s2.length()) {
+            return memo[i][j] = s1.charAt(i) + solve(s1, s2, i + 1, j, memo);
         }
 
         if(memo[i][j] != -1) {
@@ -33,19 +33,9 @@ class Solution {
             return memo[i][j] = solve(s1, s2, i + 1, j + 1, memo);
         }
 
-        int skip1 = (int)s1.charAt(i) + solve(s1, s2, i + 1, j, memo);
-        int skip2 = (int)s2.charAt(j) + solve(s1, s2, i, j + 1, memo);
+        int skip1 = s1.charAt(i) + solve(s1, s2, i + 1, j, memo);
+        int skip2 = s2.charAt(j) + solve(s1, s2, i, j + 1, memo);
 
         return memo[i][j] = Math.min(skip1, skip2);
-    }
-
-    private int deleteAll(String s, int i) {
-        int delete = 0;
-
-        for(int j = i; j < s.length(); j++) {
-            delete += s.charAt(j);
-        }
-
-        return delete;
     }
 }
