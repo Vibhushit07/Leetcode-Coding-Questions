341dd6152905e4071e0123109f4eb83eaa0e1143
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2026-01-19 21:28:17 +0530

    Time: 23 ms (13.71%), Space: 57.4 MB (79.19%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -4,11 +4,39 @@ class Solution {
         int m = mat.length, n = mat[0].length;
         int maxSide = 0;
 
+        int[][] prefixSum = new int[m][n];
+
+        prefixSum[0][0] = mat[0][0];
+
+        for(int j = 1; j < n; j++) {
+            prefixSum[0][j] = mat[0][j] + prefixSum[0][j - 1];
+        }
+
+        for(int i = 1; i < m; i++) {
+            prefixSum[i][0] = mat[i][0] + prefixSum[i - 1][0];
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                prefixSum[i][j] = prefixSum[i - 1][j] 
+                    + prefixSum[i][j - 1]
+                    - prefixSum[i - 1][j - 1]
+                    + mat[i][j];
+            }
+        }
+
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 int offset = Math.min(m - i, n - j);
                 for(int k = 0; k < offset; k++) {
-                    if(findSum(mat, i, j, i + k, j + k) <= threshold) {
+                    int r = i + k, c = j + k;
+                    int sum = prefixSum[r][c];
+
+                    if(i > 0) sum -= prefixSum[i - 1][c];
+                    if(j > 0) sum -= prefixSum[r][j - 1];
+                    if(i > 0 && j > 0) sum += prefixSum[i - 1][j - 1];
+
+                    if(sum <= threshold) {
                         maxSide = Math.max(maxSide, k + 1);
                     } else {
                         break;
@@ -20,15 +48,4 @@ class Solution {
         return maxSide;
     }
 
-    private int findSum(int[][] mat, int i, int j, int m, int n) {
-        int sum = 0;
-
-        for(int r = i; r <= m; r++) {
-            for(int c = j; c <= n; c++) {
-                sum += mat[r][c];
-            }
-        }
-
-        return sum;
-    }
 }
