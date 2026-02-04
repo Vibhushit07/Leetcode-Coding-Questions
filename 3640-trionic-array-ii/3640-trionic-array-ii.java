37287ea3077d50c93884c93533ae6a3d33ec9d8d
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2026-02-04 19:48:23 +0530

    Time: 21 ms (15.38%), Space: 107.9 MB (12.82%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.java
+++ 3640-trionic-array-ii/3640-trionic-array-ii.java
@@ -1,13 +1,60 @@
 class Solution {
     public long maxSumTrionic(int[] nums) {
+        int n = nums.length;
 
-        long[][] memo = new long[4][nums.length];
+        long[][] dp = new long[4][n + 1];
 
-        for(long[] mem : memo) {
-            Arrays.fill(mem, Long.MIN_VALUE);
+        for(int trend = 0; trend < 4; trend++) {
+            dp[trend][n] = (trend == 3) ? 0L : Long.MIN_VALUE;
         }
 
-        return trionic(memo, nums, nums.length, 0, 0);
+        for(int i = n - 1; i >= 0; i--) {
+            for(int trend = 0; trend < 4; trend++) {
+                long skip = Long.MIN_VALUE / 2, take = Long.MIN_VALUE / 2;
+
+                if(trend == 0) {
+                    skip = dp[0][i + 1];
+                }
+
+                if(trend == 3) {
+                    take = nums[i];
+                }
+
+                if(i + 1 < n) {
+                    int curr = nums[i];
+                    int next = nums[i + 1];
+
+                    if(trend == 0 && curr < next) {
+                        take = Math.max(take, curr + dp[1][i + 1]);
+                    }
+
+                    if(trend == 1 && curr < next) {
+                        take = Math.max(take, curr + dp[1][i + 1]);
+                    }
+
+                    if(trend == 1 && curr > next) {
+                        take = Math.max(take, curr + dp[2][i + 1]);
+                    }
+
+                    if(trend == 2 && curr > next) {
+                        take = Math.max(take, curr + dp[2][i + 1]);
+                    }
+
+                    if(trend == 2 && curr < next) {
+                        take = Math.max(take, curr + dp[3][i + 1]);
+                    }
+
+                    if(trend == 3 && curr < next) {
+                        take = Math.max(take, curr + dp[3][i + 1]);
+                    }
+
+                }
+
+                dp[trend][i] = Math.max(skip, take);
+            }
+        }
+
+        return dp[0][0];
     }
 
     long trionic(long[][] memo, int[] nums, int n, int i, int trend) {
