7f4328e0c9f9cbfee17c1fc88c1fbcab8ab1adcd
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2026-02-22 14:19:54 +0530

    Time: 1 ms (75.39%), Space: 44 MB (74.45%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -1,31 +1,29 @@
 class Solution {
     public int minHeightShelves(int[][] books, int shelfWidth) {
         int n = books.length;
-        int[][] memo = new int[n][shelfWidth + 1];
+        int[] dp = new int[n + 1];
 
-        for(int[] m : memo) {
-            Arrays.fill(m, -1);
+        for(int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            int currH = 0;
+            int currW = 0;
+
+            for(int j = i - 1; j >= 0; j--) {
+                int h = books[j][1];
+                int w = books[j][0];
+
+                if(currW + w > shelfWidth) {
+                    break;
+                }
+
+                currW += w;
+                currH = Math.max(currH, h);
+
+                int shelfH = dp[j] + currH;
+                dp[i] = Math.min(dp[i], shelfH);
+            }
         }
 
-        return solve(memo, books, shelfWidth, 0, 0, shelfWidth);    
-    }
-
-    private int solve(int[][] memo, int[][] books, int shelfWidth, int i, int shelfHeight, int remWidth) {
-        if(i >= books.length) return shelfHeight;
-
-        if(memo[i][remWidth] != -1) return memo[i][remWidth];
-
-        int bookW = books[i][0];
-        int bookH = books[i][1];
-
-        int keep = Integer.MAX_VALUE;
-        if(bookW <= remWidth) {
-            keep = solve(memo, books, shelfWidth, i + 1, Math.max(shelfHeight, bookH), 
-                remWidth - bookW);
-        }
-
-        int skip = shelfHeight + solve(memo, books, shelfWidth, i + 1, bookH, shelfWidth - bookW);
-
-        return memo[i][remWidth] = Math.min(keep, skip);
+        return dp[n];
     }
 }
