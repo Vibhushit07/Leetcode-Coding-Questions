74fc8c0ace240eae740eddfec1dc14b5bcc42093
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2026-03-10 00:36:26 +0530

    Time: 147 ms (78.95%), Space: 47.1 MB (85.53%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
@@ -4,37 +4,33 @@ class Solution {
 
     public int numberOfStableArrays(int zero, int one, int limit) {
 
-        int[][][] memo = new int[zero + 1][one + 1][2];
+        int[][][] dp = new int[zero + 1][one + 1][2];
 
-        for(int[][] z : memo) {
-            for(int[] o : z) {
-                Arrays.fill(o, -1);
+        dp[0][0][0] = 1;
+        dp[0][0][1] = 1;
+
+        for(int zeroLeft = 0; zeroLeft <= zero; zeroLeft++) {
+            for(int oneLeft = 0; oneLeft <= one; oneLeft++) {
+                if(zeroLeft == 0 && oneLeft == 0) continue;
+
+                int result = 0;
+
+                for(int i = 1; i <= Math.min(zeroLeft, limit); i++) {
+                    result = (result + dp[zeroLeft - i][oneLeft][0]) % M;
+                }
+
+                dp[zeroLeft][oneLeft][1] = result;
+                
+                result = 0;
+
+                for(int i = 1; i <= Math.min(oneLeft, limit); i++) {
+                    result = (result + dp[zeroLeft][oneLeft - i][1]) % M;
+                }
+
+                dp[zeroLeft][oneLeft][0] = result;
             }
         }
 
-        int startWithOne = solve(zero, one, 1, limit, memo);
-        int startWithZero = solve(zero, one, 0, limit, memo);
-
-        return (startWithOne + startWithZero) % M;
-    }
-
-    int solve(int zeroLeft, int oneLeft, int lastWasOne, int limit, int[][][] memo) {
-        if(zeroLeft == 0 && oneLeft == 0) return 1;
-
-        if(memo[zeroLeft][oneLeft][lastWasOne] != -1) return memo[zeroLeft][oneLeft][lastWasOne];
-
-        int result = 0;
-
-        if(lastWasOne == 1) {
-            for(int i = 1; i <= Math.min(zeroLeft, limit); i++) {
-                result = (result + solve(zeroLeft - i, oneLeft, 0, limit, memo)) % M;
-            }
-        } else {
-            for(int i = 1; i <= Math.min(oneLeft, limit); i++) {
-                result = (result + solve(zeroLeft, oneLeft - i, 1, limit, memo)) % M;
-            }
-        }
-
-        return memo[zeroLeft][oneLeft][lastWasOne] = result;
+        return (dp[zero][one][0] + dp[zero][one][1]) % M;
     }
 }
